f9fd1b271ceec84291451f121efa72da52c03fd4
Leonardo Azevedo <83608386+Azevedo1996@users.noreply.github.com>
2023-06-01 21:33:17 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,50 +1,23 @@
 import streamlit as st
-import requests
-from bs4 import BeautifulSoup
 
+def main():
+    st.title("Gerador de Lista de Compras Semanal")
+    
+    # Campo de entrada de URL na barra lateral
+    url = st.sidebar.text_input("Insira a URL do site de receitas", value="", key='urls')
+    
+    # Verifica se o campo de URL não está vazio
+    if url:
+        st.write("URL inserida:", url)
+        # Lógica para processar a URL e gerar a lista de compras
+        # ...
+        # Exemplo de geração da lista de compras
+        lista_compras = ["Ingredientes A", "Ingredientes B", "Ingredientes C"]
+        
+        st.subheader("Lista de Compras:")
+        for item in lista_compras:
+            st.write(item)
+    
 
-def obter_ingredientes(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    ingredientes = []
-    for ingrediente in soup.find_all('li', class_='ingredient'):
-        ingredientes.append(ingrediente.text.strip())
-    return ingredientes
-
-
-def gerar_lista_compras(receitas):
-    lista_compras = []
-    for receita in receitas:
-        ingredientes = obter_ingredientes(receita)
-        lista_compras.extend(ingredientes)
-    return lista_compras
-
-
-# Cabeçalho do aplicativo Streamlit
-st.title("Gerador de Lista de Compras")
-st.write("Selecione 7 receitas para a semana e gere a lista de compras de ingredientes.")
-
-# Verifica se é o primeiro acesso à aplicação
-if 'urls' not in st.session_state:
-    st.session_state.urls = []
-    st.session_state.urls = st.text_input("Insira a URL do site de receitas:", value="", key='urls')
-else:
-    st.write("URLs salvas:")
-    st.write(st.session_state.urls)
-    if st.button("Editar URLs"):
-       st.session_state.urls = st.text_input("Insira a URL do site de receitas:", st.session_state.urls, key='urls')
-
-# Lista de seleção das receitas
-receitas_selecionadas = st.multiselect("Selecione as receitas:", st.session_state.urls, default=st.session_state.urls[:7])
-
-# Verifica se o usuário selecionou 7 receitas
-if len(receitas_selecionadas) == 7:
-    # Gera a lista de compras
-    lista_compras_semana = gerar_lista_compras(receitas_selecionadas)
-
-    # Imprime a lista de compras
-    st.subheader("Lista de Compras:")
-    for item in lista_compras_semana:
-        st.write("- " + item)
-else:
-    st.warning("Por favor, selecione exatamente 7 receitas.")
+if __name__ == '__main__':
+    main()
